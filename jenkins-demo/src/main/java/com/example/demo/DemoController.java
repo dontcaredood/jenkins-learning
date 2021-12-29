8474f534e6e3de24a01647072abdd2389c9e1180
SanthoshKumar Pandi <SanthoshKumar.Pandi@lntinfotech.com>
2021-12-29 12:36:19 +0530

    Sysout added

--- jenkins-demo/src/main/java/com/example/demo/DemoController.java
+++ jenkins-demo/src/main/java/com/example/demo/DemoController.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.*;
 public class DemoController {
 	@RequestMapping(value="", produces="application/json")
 	public String welcome() {
+		System.out.println("Demo Application running!");
 		return "Welcome to Jenkins learning Buddy!";
 	}
 }
